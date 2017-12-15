6febee679a6944cce5abac6c0380c069826315a0
Brian Cilenti <briancilenti@gmail.com>
2017-12-15 13:37:08 -0800

    completed thru lecture 133; conditionally making request for a specific post in componentWillUpdate only if we're fetching a new post and preventing infinite loop by also checking that we only fetch when a new post is clicked

--- src/components/FullPost/FullPost.js
+++ src/components/FullPost/FullPost.js
@@ -1,20 +1,46 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import './FullPost.css';
 
 class FullPost extends Component {
-    render () {
-        let post = <p>Please select a Post!</p>;
-        post = (
-            <div className="FullPost">
-                <h1>Title</h1>
-                <p>Content</p>
-                <div className="Edit">
-                    <button className="Delete">Delete</button>
-                </div>
-            </div>
 
-        );
+    state = {
+        loadedPost: null
+    }
+    // here we will use axios.get, but the url has to target the specific
+    // post object which we want to display the properties of in the full
+    // post.
+    componentDidUpdate () {
+        if (this.props.id) {
+            // we will get an infinite loop if we don't add a check to make sure
+            // that we only send a GET request when loading a new post id.
+            if ( !this.state.loadedPost || (this.state.loadedPost && this.state.loadedPost.id !== this.props.id) ) {
+                axios.get('https://jsonplaceholder.typicode.com/posts/' + this.props.id)
+                    .then(response =>{
+                        //console.log(response);
+                        this.setState({loadedPost: response.data});
+                    });
+            }
+        }
+    }
+
+    render () {
+        let post = <p style={{textAlign: 'center'}}>Please select a Post!</p>;
+        if (this.props.id) {
+            post = <p style={{textAlign: 'center'}}>loading...!</p>;
+        }
+        if (this.state.loadedPost) {
+            post = (
+                <div className="FullPost">
+                    <h1>{this.state.loadedPost.title}</h1>
+                    <p>{this.state.loadedPost.body}</p>
+                    <div className="Edit">
+                        <button className="Delete">Delete</button>
+                    </div>
+                </div>
+            );
+        }
         return post;
     }
 }
--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,21 +1,67 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
 import NewPost from '../../components/NewPost/NewPost';
 import './Blog.css';
 
+// XMLHttpRequest - default javaScript object for making requests
+// we will use third party AXIOS to help us do this.
+
 class Blog extends Component {
+    state = {
+        posts: [],
+        selectedPostId: null
+    }
+    // componentDidMount is the best place to send HTTP requests
+    // cannot store get request results in a constant because javaScript
+    // executes code asyncronusly, meaning, it does not wait for the get
+    // request to finish before moving on to the next line. Axios uses
+    // promisies, a new-ish javaScript feature.
+    // then() takes a function as an input, and executes once the
+    // axios.get() request has completed. The function inside then() recieves
+    // a response object as an input.
+    componentDidMount () {
+        axios.get('https://jsonplaceholder.typicode.com/posts')
+            .then(response => {
+                // transform data after recieving to limit number of
+                // posts displayed.
+                const posts = response.data.slice(0, 4);
+                // distributing the content of posts, one by one via map,
+                // and hardcoding a new author property to 'Max' on each
+                const updatedPosts = posts.map(post => {
+                    return {
+                        ...post,
+                        author: 'Max'
+                    }
+                });
+                this.setState({posts: updatedPosts});
+                // this.setState({posts: response.data});
+                // console.log( response );
+            });
+    }
+
+    postSelectedHandler = (id) => {
+        this.setState({selectedPostId: id});
+    }
+
     render () {
+        const posts = this.state.posts.map(post => {
+            return <Post
+                key={post.id}
+                title={post.title}
+                author={post.author}
+                clicked={() => this.postSelectedHandler(post.id)} />
+        });
+
         return (
             <div>
                 <section className="Posts">
-                    <Post />
-                    <Post />
-                    <Post />
+                    {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPostId} />
                 </section>
                 <section>
                     <NewPost />
